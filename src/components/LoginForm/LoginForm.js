146b69d179651cd7d5a6d21f2e58abce65e5b7ad
Michael Oldacre <mloldacre@live.com>
2020-08-28 12:22:49 -0400

    Updated login form and services

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import TokenService from '../../services/token-service'
 import { Button, Input } from '../Utils/Utils'
+import AuthApiService from '../../services/auth-api-services'
 
 export default class LoginForm extends Component {
   static defaultProps = {
@@ -23,13 +24,33 @@ export default class LoginForm extends Component {
     password.value = ''
     this.props.onLoginSuccess()
   }
+  
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    this.setState({ error: null })
+    const { user_name, password } = ev.target
+
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then(res => {
+        user_name.value = ''
+        password.value = ''
+        TokenService.saveAuthToken(res.authToken)
+        this.props.onLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  }
 
   render() {
     const { error } = this.state
     return (
       <form
         className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
+        onSubmit={this.handleSubmitJwtAuth}
       >
         <div role='alert'>
           {error && <p className='red'>{error}</p>}
